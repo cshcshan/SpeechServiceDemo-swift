adc000d506121c3c8c23052557e53324f4ff0157
Han <csh771355@gmail.com>
2020-03-03 23:41:03 +0800

    ignore Pods/ (git rm --cached -r <dir>)

--- Pods/Target Support Files/Pods-SpeechTranslation-FA/Pods-SpeechTranslation-FA-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SpeechTranslation_FA : NSObject
-@end
-@implementation PodsDummy_Pods_SpeechTranslation_FA
-@end
--- Pods/Target Support Files/Pods-speechservicedemo/Pods-speechservicedemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_speechservicedemo : NSObject
-@end
-@implementation PodsDummy_Pods_speechservicedemo
-@end
